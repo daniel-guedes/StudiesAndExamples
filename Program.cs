223214960855dd47bf7254b845b5d30bed282976
daniel-guedes <daniel.metall@gmail.com>
2024-03-09 21:34:36 -0300

    Criado a model ToDo usando record, onde suas propriedades são declaradas diretamente no construtor. Realizado um teste de funcionamento da model.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => {
+	var todo = new ToDo(Guid.NewGuid(), "Criar testes automatizados", false);
+	return Results.Ok(todo);
+});
 
 app.Run();
